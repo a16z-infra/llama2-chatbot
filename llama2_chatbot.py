0296cf6501e4d25a5640631b1a9e8eff68d9e91e
Joshua Carroll <joshua.carroll@snowflake.com>
2023-07-18 14:35:06 -0700

    Add a warning if .env is missing

--- llama2_chatbot.py
+++ llama2_chatbot.py
@@ -55,6 +55,10 @@ REPLICATE_MODEL_ENDPOINT7B = os.environ.get('REPLICATE_MODEL_ENDPOINT7B', defaul
 REPLICATE_MODEL_ENDPOINT13B = os.environ.get('REPLICATE_MODEL_ENDPOINT13B', default='')
 PRE_PROMPT = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as Assistant."
 
+if not (REPLICATE_API_TOKEN and REPLICATE_MODEL_ENDPOINT13B and REPLICATE_MODEL_ENDPOINT7B):
+    st.warning("Add a `.env` file to your app directory with the keys specified in `.env_template` to continue.")
+    st.stop()
+
 #container for the chat history
 response_container = st.container()
 #container for the user's text input
